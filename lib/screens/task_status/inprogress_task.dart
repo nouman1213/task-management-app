e254cca0f0ff4623bfd446f52ecc13c369deb3e2
nouman1213 <noumanashraf1213@gmail.com>
2023-05-23 18:29:10 +0500

    changes

---
+++ lib/screens/task_status/inprogress_task.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:task_management_app/components/tasktile.dart';
+
+import '../../controller/task/addTaskController.dart';
+import '../../model/taskmodel.dart';
+import '../taskDetails_screen.dart';
+
+class InProgressScreen extends StatelessWidget {
+  final AddTaskController addTaskController;
+
+  InProgressScreen({required this.addTaskController});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('In Progress Tasks'),
+      ),
+      body: Obx(
+        () => addTaskController.inProgressTasks.isEmpty
+            ? Center(
+                child: Text("No Task in Progress"),
+              )
+            : ListView.builder(
+                itemCount: addTaskController.inProgressTasks.length,
+                itemBuilder: (context, index) {
+                  Task task = addTaskController.inProgressTasks[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GestureDetector(
+                        onTap: () {
+                          Get.to(() => TaskDetailScreen(
+                              task: task,
+                              addTaskController: addTaskController));
+                        },
+                        child: TaskTile(task: task)),
+                  );
+                },
+              ),
+      ),
+    );
+  }
+}
